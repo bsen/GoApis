9572074e2d991a1455258627fdd936afc7bc9493
bsen <biswaasen@gmail.com>
2025-04-06 22:20:19 +0530

    goapi

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module go-apis
+module goapi
 
 go 1.24.1
--- server.go
+++ server.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	userController "goapi/controller"
 	"log"
 	"net/http"
-    "go-apis/controller"
 )
 
 func main(){
