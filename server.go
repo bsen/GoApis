82411ba2c04b39c383431c7439ab6317f7c9f50d
bsen <biswaasen@gmail.com>
2025-04-06 23:56:45 +0530

    update

--- server.go
+++ server.go
@@ -9,12 +9,12 @@ import (
 
 func main(){
 	var port string = "8002"
-	http.HandleFunc("/signup",SignUp)
+	http.HandleFunc("/users",DummyUsers)
 	fmt.Printf("http://localhost:%v\n",port)
 	log.Fatal(http.ListenAndServe(":" + port,nil))	
 }
 
-func SignUp(w http.ResponseWriter, r * http.Request){
+func DummyUsers(w http.ResponseWriter, r * http.Request){
 	var email string = r.URL.Query().Get("email")
     var pass string = r.URL.Query().Get("pass")
     userController.HandleSignUp(email,pass)
